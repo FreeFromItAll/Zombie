33471b811a3ba63ece21a664ff1976fb5558a138
Daniel <daniel.robin22@gmail.com>
2015-08-30 19:40:31 -0400

    added main

---
+++ zombie.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(const int argc, const char** argv)
+{
+	if(argc < 2) {
+usage:
+		puts("usage: zombie [-i|-f] file");
+		exit(1);
+	}
+
+	puts("Zombie Script Interpreter v1.3.1");
+	puts("--------------------------------");
+
+	
+}
